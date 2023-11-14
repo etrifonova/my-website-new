4a19e3c7a89c388098f2eee0fcc47b3c76b5e239
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-11-14 13:29:28 +0300

    feat: add cta

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import './cta.css';
+import React from "react";
+import "./cta.css";
 
 const CTA = () => {
   return (
     <div>
-        CTA
+      <div className="gpt3__cta section__padding">
+        <div className="gpt3__cta-content">
+          <p>Request Early Access to Get Started</p>
+          <h3>Register today & start exploring the endless possibilities.</h3>
+        </div>
+        <div className="gpt3__cta-button">
+          <button type="button">Get Started</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
